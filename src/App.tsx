90d75a4783c8a0e968658153a4e391287ece8970
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2024-07-29 09:21:08 +0100

    Added Vercel and Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Homepage from "./components/Homepage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <div className="">
         <Homepage />
+        <Analytics />
       </div>
     </>
   );
