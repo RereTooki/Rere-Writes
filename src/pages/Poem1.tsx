74946371d75b00f53ed6e461db29033bab8ff11a
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-06 09:27:35 +0100

    standbyme bg image for the poem

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Testing from "../components/Testing";
 
 const Poem1 = () => {
-  return <div>Poem1</div>;
+  return (
+    <>
+      <div className="bg-poem1BG h-[100vh] bg-[length:100vw_280vh] bg-no-repeat bg-center w3-animate-opacityIntro">
+        Poem1
+      </div>
+    </>
+  );
 };
 
 export default Poem1;
