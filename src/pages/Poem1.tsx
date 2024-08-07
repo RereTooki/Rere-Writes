402992edc7f3a0912c82d61394e935b49566a0f9
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-07 10:39:17 +0100

    testing

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -8,7 +8,7 @@ const Poem1 = () => {
         <div className="quoteBG quoteBGP1 h-[50vh] lg:h-[50vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] px-[10vw] srounded-tr-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl border-2 w-auto tab:w-[40vw]">
           {/* <p className="exoFont relastive top-[45%]">“Quote 1” - Speaker 1</p> */}
         </div>
-        <div className="border-2 h-[50vh] tab:w-[40vw]"></div>
+        <div className="border-2 h-[50vh] tab:w-[40vw]">rere</div>
       </div>{" "}
     </>
   );
