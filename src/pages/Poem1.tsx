177f622291b25c774926ede9aa94547e61eed1ed
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-07 12:48:33 +0100

    removed quotebgs

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -7,7 +7,7 @@ const Poem1 = () => {
     <>
       <div className="poemGrad tab:h-[100vh] flex flex-col tab:flex-row  tab:items-center tab:justify-center">
         <div className="tab:bg-white  tab:rounded-tl-[20px]  tab:rounded-bl-[20px] ">
-          <div className="quoteBG quoteBGP1 h-[50vh] tab:h-[85vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] tab:rounded-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl bsorder-2 w-auto tab:w-[40vw] bsorder-4 border-black overflow-hidden">
+          <div className="quoteBGs quoteBGP1 h-[50vh] tab:h-[85vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] tab:rounded-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl bsorder-2 w-auto tab:w-[40vw] bsorder-4 border-black overflow-hidden">
             <picture className="hidden tab:block  w-full h-full">
               <source className="" media="(min-width: 565px)" srcSet={SBMBG} />
               <img
