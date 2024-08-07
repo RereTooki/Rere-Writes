578e9e771a612a621a61684cbc7e0d8858b6d6d6
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-07 10:43:13 +0100

    code should break

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -4,11 +4,11 @@ import Testing from "../components/Testing";
 const Poem1 = () => {
   return (
     <>
-      <div className="poemGrad h-[100vh] flex flex-col tab:flex-row xmd:px-[5vw] tab:px-0 tab:items-center tab:justify-center">
+      <div className="poemGrad h-[s100vh] flex flex-col tab:flex-row xmd:px-[5vw] tab:px-0 tab:items-center tab:justify-center">
         <div className="quoteBG quoteBGP1 h-[50vh] lg:h-[50vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] px-[10vw] srounded-tr-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl border-2 w-auto tab:w-[40vw]">
           {/* <p className="exoFont relastive top-[45%]">“Quote 1” - Speaker 1</p> */}
         </div>
-        <div className="border-2 h-[50vh] tab:w-[40vw]">rere</div>
+        <div className="border-2 h-[80vh] tab:w-[40vw]">reres</div>
       </div>{" "}
     </>
   );
