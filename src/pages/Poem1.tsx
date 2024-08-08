ecb0f21480321345f75820454abc77b44c0ce85e
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-08 10:23:27 +0100

    gave it borders

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -6,9 +6,9 @@ import SBMM from "../assets/images/poemBgs/SBM-M.jpg";
 const Poem1 = () => {
   return (
     <>
-      <div className="poemBG poemBG-SBM tab:h-[100vh] flex flex-col tab:flex-row  tab:items-center tab:justify-center ">
-        <div className="tab:bg-white tab:rounded-tl-[20px] tab:rounded-bl-[20px] w3-animate-left">
-          <div className="quoteBGs quoteBGP1 h-[50vh] tab:h-[85vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] tab:rounded-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl tab:drop-shadow-4xl bsorder-2 w-auto tab:w-[45vw] lg:w-[40vw] bsorder-4 border-black overflow-hidden">
+      <div className="poemBG poemBG-SBM tab:h-[100vh] flex flex-col tab:flex-row  tab:items-center tab:justify-center border-4 border-black border-double">
+        <div className="tab:bg-white tab:rounded-tl-[20px] tab:rounded-bl-[20px] w3-animate-left  ">
+          <div className="quoteBGs quoteBGP1 h-[50vh] tab:h-[85vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] tab:rounded-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl tab:drop-shadow-4xl bsorder-2 w-auto tab:w-[45vw] lg:w-[40vw] border-4 border-black border-double overflow-hidden">
             <picture className="hidden tab:block  w-full h-full">
               <source className="" media="(min-width: 565px)" srcSet={SBMBG} />
               <img
@@ -19,7 +19,7 @@ const Poem1 = () => {
             </picture>
           </div>
         </div>
-        <div className="sborder-4 h-[80vh] tab:h-[85vh] tab:w-[45vw] lg:w-[40vw] tab:bg-white  tab:rounded-tr-[20px]  tab:rounded-br-[20px] px-[10vw] tab:px-[10px] w3-animate-right">
+        <div className="sborder-4 h-[80vh] tab:h-[85vh] tab:w-[45vw] lg:w-[40vw] tab:bg-white  tab:rounded-tr-[20px]  tab:rounded-br-[20px] px-[10vw] tab:px-[10px] w3-animate-right ">
           rere
         </div>
       </div>{" "}
