51969e0194dfac68dac84a22075ee0ecab6b9d4b
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-08 10:04:15 +0100

    added a new bg

--- src/pages/Poem1.tsx
+++ src/pages/Poem1.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Testing from "../components/Testing";
 import SBMBG from "../assets/images/poemBgs/poem1Bg.png";
+import SBMM from "../assets/images/poemBgs/SBM-M.jpg";
 
 const Poem1 = () => {
   return (
     <>
-      <div className="poemGrad tab:h-[100vh] flex flex-col tab:flex-row  tab:items-center tab:justify-center ">
+      <div className="poemBG poemBG-SBM tab:h-[100vh] flex flex-col tab:flex-row  tab:items-center tab:justify-center ">
         <div className="tab:bg-white tab:rounded-tl-[20px] tab:rounded-bl-[20px] w3-animate-left">
           <div className="quoteBGs quoteBGP1 h-[50vh] tab:h-[85vh] flex flex-col  text-center text-white text-[14px] md:text-[16px] nxl:text-[22px] tab:rounded-[20px] rounded-b-[40px] w3-animate-opacitys drop-shadow-2xl tab:drop-shadow-4xl bsorder-2 w-auto tab:w-[45vw] lg:w-[40vw] bsorder-4 border-black overflow-hidden">
             <picture className="hidden tab:block  w-full h-full">
