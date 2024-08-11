10e50f7dc3388ad7f1060d3b7970edbe68f69069
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-11 23:03:49 +0100

    started about me section

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -8,13 +8,14 @@ import v1RDesk from "../assets/icons/bg-pattern-intro-right-desktop.svg";
 import WritingAnimation from "../animations/WritingAnimation";
 import LeftDivAnimation from "../animations/LeftDivAnimation";
 import RightDivAnimation from "../animations/RightDivAnimation";
+import mp3 from "../assets/images/MyPortrait.png";
 
 const Testing = () => {
   return (
     <>
-      <div className="border-4 mb-10 h-[100vh] introGrads aboutMeGrad exoFont select-none w3-animate-opacityIntro pt-10 border-black flex flex-col justify-between items-centers px-[3vw]">
+      <div className="sborder-4 mb-10 h-[100vh] introGrads aboutMeGrad exoFont select-none w3-animate-opacityIntro pt-10 tab:pt-[80px] border-black flex flex-col justify-between items-centers px-[3vw] overflow-x-hidden">
         <div
-          className="w3-container pb-[48px] border-black border-4"
+          className="w3-containers pb-[48px] border-black sborder-4"
           id="projects"
         >
           <h3
@@ -25,9 +26,26 @@ const Testing = () => {
             <WritingAnimation text="About Me!" />
           </h3>
         </div>
-        <div className="border-2 border-black h-full sgradient-border px-[3vw]">
-          rere div 1
-          <WritingAnimation text="Hello, world!" />
+        <div className="border-2 border-[#7f7fd5] h-full sgradient-border px-[3vw] flex flex-col items-center justify-center">
+          <h2 className="text-[16vw] md:text-[60px] xmd:text-[70px] tab:text-[80px] nxl:text-[100px] exoFont text-center font-semibold md:leading-tight">
+            <a
+              href="https://www.linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+              className="underline underline-offset-2 hover:underline-offset-4 hover:scale-[1.02] meriendaFont bg-clip-text text-transparent bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] hover:bg-gradient-to-l font-bold"
+              target="_blank"
+            >
+              Rerel'Oluwa Tooki
+            </a>
+          </h2>
+          <div className="w-[45vw] h-[45vw] md:w-[160px] md:h-[160px] tab:w-[180px] tab:h-[180px]  nxl:w-[220px] nxl:h-[220px] w3-animate-zoom w3-animate-opacityIntrog z-10">
+            <picture className="">
+              <source className="" media="(min-width: 565px)" srcSet={mp3} />
+              <img
+                src={mp3}
+                alt="Rere Writes Logo"
+                className="w-full h-full object-cover"
+              />
+            </picture>
+          </div>
         </div>{" "}
       </div>
     </>
