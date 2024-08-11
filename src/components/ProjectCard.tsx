48d90db6cd8f3c5002690a620307a8e6f89fd9a6
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-11 15:21:40 +0100

    fixed the animation & scale issue

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,25 +17,25 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="w3-col l3 m6 w3-margin-bottom">
-      <div className="w3-display-container hover:scale-[1.03]">
-        <NavLink
-          to={route}
-          target="_blank"
-          data-aos="zoom-in-up"
-          data-aos-duration="1500"
-        >
-          <div className="w3-display-topleft bg-white hover:bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] w3-padding rounded-br-lg rounded-tl-lg border-t-2 border-l-2 border-clrtransparentgreen cursor-pointer hover:border-0">
-            <p className=" hover:text-white bg-clip-text text-transparent bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] font-medium">
-              {title}
-            </p>
-          </div>
-
-          <img
-            src={imgSrc}
-            alt={imgAlt}
-            className="w-full h-[250px] object-cover tabImg2:h-auto tabImg2:object-fill nxl:h-[1s85px] nxl:w-[2s34px] rounded-lg"
-          />
-        </NavLink>
+      <div
+        className="w3-display-container"
+        data-aos="zoom-in-up"
+        data-aos-duration="1500"
+      >
+        <div className="hover:scale-[1.03]">
+          <NavLink to={route} target="_blank">
+            <div className="w3-display-topleft bg-white hover:bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] w3-padding rounded-br-lg rounded-tl-lg border-t-2 border-l-2 border-clrtransparentgreen cursor-pointer hover:border-0">
+              <p className=" hover:text-white bg-clip-text text-transparent bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] font-medium">
+                {title}
+              </p>
+            </div>
+            <img
+              src={imgSrc}
+              alt={imgAlt}
+              className="w-full h-[250px] object-cover tabImg2:h-auto tabImg2:object-fill nxl:h-[1s85px] nxl:w-[2s34px] rounded-lg"
+            />
+          </NavLink>
+        </div>
       </div>
     </div>
   );
