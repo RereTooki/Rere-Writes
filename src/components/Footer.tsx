bbbad29ae3476dab765715d08f983cb15d7c8f6d
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-18 00:43:56 +0100

    done with footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,8 @@ import "../App.css";
 import logo from "../assets/images/logo2.png";
 import { SiGmail } from "react-icons/si";
 import { FaWhatsapp } from "react-icons/fa";
+import { SiTelegram } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const email = "rereloluwa.afolabi.tooki@gmail.com"; // Replace with the recipient's email address
@@ -11,8 +13,10 @@ const Footer = () => {
     window.location.href = `mailto:${email}`;
   };
 
-  const phoneNumber = "+2347044173445"; // Replace with the recipient's phone number
-  const message = encodeURIComponent("Hello, I'm interested in OACoker&Co."); // Encode message
+  const phoneNumber = "+2348123717074"; // Replace with the recipient's phone number
+  const message = encodeURIComponent(
+    "Hello Rere, I'm interested in a collaboration!"
+  ); // Encode message
 
   const handleWhatsAppClick = () => {
     window.open(`https://wa.me/${phoneNumber}?text=${message}`, "_blank");
@@ -32,59 +36,30 @@ const Footer = () => {
               className="w-full h-full object-cover"
             />
           </div>
-          <div
-            className=" flex flex-col gap-[30px] h-[100%]"
-            data-aos="zoom-in"
-            data-aos-duration="700"
-          >
-            <div className="text-white underline underline-offset-4 ">
-              About Us
-            </div>
-            <div className="flex flex-col gap-[5px]">
-              <div className="pb-[10px] text-grayish-blues tab:max-w-[350px] tab:text-start">
-                This is a law and notary public platform under the leadership of
-                over 22 years experienced supreme court of Nigeria Legal
-                Practitioner with physical office in Lagos Nigeria.
-              </div>
-            </div>
-          </div>
-          {/* <div className="border-2 flex flex-col gap-[30px] tab:max-w-[186px]">
-            <div className="text-white underline underline-offset-4 ">
-              Our Services
-            </div>
-            <div className="flex flex-col gap-[15px] text-start">
-              <div className="text-grayish-blues">
-                Administering notarial certificates for document authentication
-              </div>
-              <div className="text-grayish-blues">
-                Taking Affidavits, Oaths, and Acknowledgements
-              </div>
-              <div className="text-grayish-blues">Attesting legal documents</div>
-              <div className="text-grayish-blues">
-                Repairing property documents and Power of Attorney
-              </div>
-              <div className="pb-[10px] text-grayish-blues">
-                Witnessing events/transactions
-              </div>
-            </div>
-          </div> */}
+
           <div
             className=" flex flex-col gap-[30px]  tab:max-w-[240px]"
             data-aos="zoom-in"
             data-aos-duration="700"
           >
             <div className="text-white underline underline-offset-4 ">
-              Contact Us
+              Contact Me
             </div>
-            <div className="flex flex-col gap-[5px] text-start">
+            <div className="flex flex-col gap-[5px] text-white text-start">
               <div className="text-grayish-blues">
-                Telephone: +234 803 313 6313
+                Telephone: +234 812 371 7074
               </div>
               <div className="text-grayish-blues">
-                WhatsApp: +234 704 417 3445
+                LinkedIn:{" "}
+                <a href="https://www.linkedin.com/in/rerel-oluwa-tooki-b53396253/">
+                  Rerel'Oluwa Tooki
+                </a>
+              </div>
+              <div className="text-grayish-blues">
+                Email: rereafolabi@gmail.com
               </div>
               <div className="pb-[10px] text-grayish-blues">
-                Email: oacoker2@gmail.com
+                Email: rereloluwa.afolabi.tooki@gmail.com
               </div>
             </div>
           </div>
@@ -96,7 +71,7 @@ const Footer = () => {
             <div className="text-white underline underline-offset-4 ">
               Stay Connected
             </div>
-            <div className="flex flex-row istems-center justify-around">
+            <div className="flex flex-row istems-center justify-around flex-wrap max-w-[130px] gap-2 sborder-2">
               <div className="w-[2rem]  h-[2rem] ">
                 <button
                   onClick={handleEmailClick}
@@ -108,6 +83,7 @@ const Footer = () => {
                   />
                 </button>
               </div>
+
               <div className="w-[2rem]  h-[2rem] ">
                 <button
                   onClick={handleWhatsAppClick}
@@ -119,18 +95,35 @@ const Footer = () => {
                   />{" "}
                 </button>
               </div>
+              <div className="w-[2rem]  h-[2rem] ">
+                <a href="https://t.me/reretooki">
+                  <SiTelegram
+                    className="w-full h-full object-cover"
+                    color="#fff"
+                  />{" "}
+                </a>
+              </div>
+              <div className="w-[2rem]  h-[2rem] ">
+                <a href="https://www.linkedin.com/in/rerel-oluwa-tooki-b53396253/">
+                  <FaLinkedin
+                    className="w-full h-full object-cover"
+                    color="#fff"
+                  />{" "}
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="attribution pb-[30px] underline-offset-4 whitespace-nowraps text-[4.5vw] md:text-[16px]">
-          © 2024. All Rights Reserved.{" "}
+        <div className="attribution pb-[30px] underline-offset-4 whitespace-nowraps text-[4.5vw] md:text-[16px] text-center text-white px-2">
+          Designed & Developed by{" "}
           <a
-            href="https://oluyomicoker.vercel.app/"
-            className="text-[4.5vw] md:text-[16px]"
+            href="https://rere-writes.vercel.app/"
+            className="text-[4.5vw] md:text-[16px] underline decoration-dotted"
             target="_blank"
           >
-            O A COKER & CO.
-          </a>
+            Rerel'Oluwa Tooki
+          </a>{" "}
+          © 2024. All Rights Reserved.
         </div>
       </div>
     </>
