c951c34435ec254532cf1792635f5cce6d8e38be
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2024-08-17 09:00:11 +0100

    added animations back

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,8 +13,8 @@ const AboutMe = () => {
         >
           <h3
             className="text-[25px] gradient-border-bottom exoFont pb-[4px]"
-            data-aoss="zoom-in"
-            data-aoss-duration="700"
+            data-aos="zoom-in"
+            data-aos-duration="700"
           >
             <WritingAnimation text="About Me!" />
           </h3>
@@ -25,16 +25,16 @@ const AboutMe = () => {
               <div className="border-2s border-black flex items-center justify-center overflow-hidden">
                 <span
                   className="inline-block overflow-hidden pt-1 -mr-7 sm:-mr-5 md:-mr-7 lg:-mr-9 -mb-1 sm:-mb-2 md:-mb-3 lg:-mb-4 opacity: 1; transform: none; stext-[32vw] pb-2 md:pb-4 tab:pb-5 nxl:pb-10 xl:pb-5 bg-clip-text text-transparent bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] hover:bg-gradient-to-l"
-                  data-aoss="fade-up"
-                  data-aoss-duration="1500"
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
                 >
                   TOOKI&nbsp;
                 </span>
               </div>
               <div
                 className="flex items-center justify-center overflow-hidden border-black sborder-2"
-                data-aoss="fade-down"
-                data-aoss-duration="2000"
+                data-aos="fade-down"
+                data-aos-duration="2000"
               >
                 <span className="inline-block overflow-hidden pt-1 -mr-4 sm:-mr-5 md:-mr-4 tab:-mr-7 lg:-mr-8 xl:-mr-10 -mb-1 sm:-mb-2 md:-mb-3 lg:-mb-4 opacity: 1; transform: none; text-[13.5vw] md:text-[14vw] sab:text-[13.5vw] lg:text-[14vw] xl:text-[145px] scale-y-[2] xl:scale-y-[1.55] tab:pb-5 tracking-wide xl:tracking-normal text-center bg-clip-text text-transparent bg-gradient-to-r from-[#7F7FD5] to-[#31B7C2] hover:bg-gradient-to-l">
                   REREL'OLUWA&nbsp;
@@ -45,8 +45,8 @@ const AboutMe = () => {
           <div className="sborder-2 border-black flex flex-col items-center justify-center">
             <div
               className="relative bottom-[30px] tab:bottom-[70px] nxl:bottom-[70px] w-[65vw] h-[65vw] md:w-[240px] md:h-[240px] tab:w-[300px] tab:h-[300px]  nxl:w-[410px] nxl:h-[410px] z-10 rounded-lg drop-shadow-2xl"
-              data-aoss="zoom-in"
-              data-aoss-duration="1500"
+              data-aos="zoom-in"
+              data-aos-duration="1500"
             >
               <picture className="">
                 <source className="" media="(min-width: 565px)" srcSet={mp3} />
@@ -60,8 +60,8 @@ const AboutMe = () => {
           </div>
           <div
             className="gradient-border border-black flex flex-col items-center justify-center p-4"
-            data-aoss="zoom-in"
-            data-aoss-duration="1500"
+            data-aos="zoom-in"
+            data-aos-duration="1500"
           >
             <p className="text-center">
               CompTIA Certified Cybersecurity Engineer || Frontend Engineer (Web
